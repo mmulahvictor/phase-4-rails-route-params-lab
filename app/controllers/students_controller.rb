aee2e5f8d0abc9191973ac6f2d1551c33e06d9de
mmulahvictor <mmulahvictor@gmail.com>
2022-09-26 14:24:02 +0300

    commit tree ea7dcf2683d03c6b9dea48487f9238ea39c641b2

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,7 +1,16 @@
 class StudentsController < ApplicationController
 
+  def show
+    student = Student.find(params[:id])
+    render json: student
+  end
+
   def index
-    students = Student.all
+    students = if params[:name]
+      Student.by_name(params[:name])
+    else
+      Student.all
+    end
     render json: students
   end
 
